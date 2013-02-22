c4e90b49a91a97926465967753fa1d48da9f8740
neil <hello@neilkinnish.com>
2013-02-22 21:02:10 +0000

    initial commit

---
+++ public/js/bootstrap/bootstrap.mix.js
@@ -0,0 +1,13 @@
+@import "bootstrap-transition.js";
+@import "bootstrap-alert.js";
+@import "bootstrap-button.js";
+@import "bootstrap-carousel.js";
+@import "bootstrap-collapse.js";
+@import "bootstrap-dropdown.js";
+@import "bootstrap-modal.js";
+@import "bootstrap-tooltip.js";
+@import "bootstrap-popover.js";
+@import "bootstrap-scrollspy.js";
+@import "bootstrap-tab.js";
+@import "bootstrap-typeahead.js";
+@import "bootstrap-affix.js";
